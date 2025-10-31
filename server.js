edad9f4323b3f72dcf135b68e5ef62175a32046e
Jayant-Thakre-05 <jayantthakre@gmail.com>
2025-10-31 21:52:18 +0530

    kkkk

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const cors = require("cors");
 const authRoutes = require("./src/routes/auth.routes");
 const recipeRoutes = require("./src/routes/recipe.routes");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 
+const __dirname = path.resolve();
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({origin:"https://tastemate05.netlify.app/",credentials:true}));
+app.use(express.static(path.join(__dirname, "../recipe/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../recipe/dist/index.html"));
+});
 
 connectDB();
 
